d2e92574cc970dc2eba20d7f2b464d55696f8338
Kaspars Revins <okoikwedced@gmail.com>
2023-05-01 09:23:06 +0100

    Dice Roll

--- 17-Dice_Roll.py
+++ 17-Dice_Roll.py
@@ -8,7 +8,6 @@ Exercise Description
 Write a rollDice() function with a numberOfDice parameter that represents the number of six-sided dice. 
 The function returns the sum of all of the dice rolls. For this exercise you must import Python’s random module to call its random.randint() function for this exercise.
 """
-
 def rollDice(numberOfDice):
     import random
     result = 0
