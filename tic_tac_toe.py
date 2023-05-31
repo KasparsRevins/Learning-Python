3181508d2e939c1e8a9eb4bfec088e8c2c037ccf
Kaspars Revins <okoikwedced@gmail.com>
2023-05-31 09:18:27 +0100

    Tic tac toe printing board

---
+++ tic_tac_toe.py
@@ -0,0 +1,13 @@
+
+
+# Logic
+
+
+# Printing the board 
+
+blank_space = " " * 3
+print(blank_space + "|" + blank_space + "|" + blank_space)
+print("-" * 11)
+print(blank_space + "|" + blank_space + "|" + blank_space)
+print("-" * 11)
+print(blank_space + "|" + blank_space + "|" + blank_space)
